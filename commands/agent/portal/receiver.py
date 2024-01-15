290a3c4046ea70f5f144f27cb66d7b02fc3d235f
Adrian Webb <adrian@webb.sh>
2024-01-15 05:20:38 -0500

    Updating the portal receiver agent event parameters and ensuring no duplicate events processed.

--- commands/agent/portal/receiver.py
+++ commands/agent/portal/receiver.py
@@ -17,26 +17,42 @@ class Receiver(Agent('portal.receiver')):
     def _request_events(self):
         for portal_name in self.get_portals():
             for event in self.portal_list(portal_name, 'event', 'follow'):
-                self._process_event(portal_name, event['type'], event['data'])
+                self._process_event(portal_name, event['id'], event['type'], event['data'])
 
     def receiver_retry(self):
         for package in self.listen('agent:portal:receiver:retry', state_key = 'portal_receiver'):
             event = package.message
-            self._process_event(event['portal'], event['type'], event['data'])
+            self._process_event(event['portal'], event['id'], event['type'], event['data'])
 
 
-    def _process_event(self, portal_name, event_type, event_data):
-        self.data("Portal {} event received".format(event_type), event_data)
-        self.exec_local("portal event {}".format(event_type),
-            options = {
+    def _process_event(self, portal_name, event_id, event_type, event_data):
+        if not getattr(self, 'event_index', None):
+            self.event_index = {}
+
+        if event_id not in self.event_index:
+            self.data("Portal {} event received".format(event_type), event_data)
+            try:
+                self.exec_local("portal event {}".format(event_type),
+                    options = {
+                        'portal': portal_name,
+                        'event_id': event_id,
+                        'event_fields': event_data,
+                        'async_exec': True,
+                        'verbosity': self.verbosity
+                    }
+                )
+            except Exception:
+                self.send('agent:portal:receiver:retry', {
+                    'portal': portal_name,
+                    'id': event_id,
+                    'type': event_type,
+                    'data': event_data
+                })
+
+            self.send('agent:portal:receiver:event', {
                 'portal': portal_name,
-                'event_fields': event_data,
-                'async_exec': True,
-                'verbosity': self.verbosity
-            }
-        )
-        self.send('agent:portal:receiver:event', {
-            'portal': portal_name,
-            'type': event_type,
-            'data': event_data
-        })
+                'id': event_id,
+                'type': event_type,
+                'data': event_data
+            })
+            self.event_index[event_id] = True
