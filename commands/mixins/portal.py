a6168b1a2a00ced3e5c4a3b2039b84ce836df31c
Adrian Webb <adrian@webb.sh>
2024-12-02 03:02:59 -0500

    Adding a parse_file_content function in the portal command mixin.

--- commands/mixins/portal.py
+++ commands/mixins/portal.py
@@ -8,7 +8,7 @@ import base64
 import string
 
 
-class PortalCommandMixin(CommandMixin('portal')):
+class PortalCommandMixin(CommandMixin("portal")):
 
     def get_portals(self, **filters):
         portals = []
@@ -22,66 +22,62 @@ class PortalCommandMixin(CommandMixin('portal')):
                 portals.append(portal)
         return portals
 
-    def get_portal(self, name = None):
+    def get_portal(self, name=None):
         if not name:
             name = settings.DEFAULT_PORTAL
         return Portal(self, name)
 
-
-    def portal_list(self, portal_name, data_type, operation = None, **filters):
-        return self._transmit(portal_name, data_type, 'list', {
-            'operation': operation,
-            'filters': filters
-        })
+    def portal_list(self, portal_name, data_type, operation=None, **filters):
+        return self._transmit(
+            portal_name, data_type, "list", {"operation": operation, "filters": filters}
+        )
 
     def portal_retrieve(self, portal_name, data_type, id):
-        return self._transmit(portal_name, data_type, 'retrieve', {
-            'id': id
-        })
+        return self._transmit(portal_name, data_type, "retrieve", {"id": id})
 
     def portal_create(self, portal_name, data_type, **fields):
-        return self._transmit(portal_name, data_type, 'create', {
-            'fields': fields
-        })
+        return self._transmit(portal_name, data_type, "create", {"fields": fields})
 
     def portal_update(self, portal_name, data_type, id, **fields):
-        return self._transmit(portal_name, data_type, 'update', {
-            'id': id,
-            'fields': fields
-        })
+        return self._transmit(
+            portal_name, data_type, "update", {"id": id, "fields": fields}
+        )
 
     def portal_delete(self, portal_name, data_type, id):
-        return self._transmit(portal_name, data_type, 'delete', {
-            'id': id
-        })
-
+        return self._transmit(portal_name, data_type, "delete", {"id": id})
 
     def _transmit(self, portal_name, data_type, operation, params):
-        result = self.submit('agent:portal:transmitter', {
-            'portal': portal_name,
-            'data_type': data_type,
-            'operation': operation,
-            'params': params
-        })
-        if result['status'] == 'error':
-            raise PortalConnectionError(result['error'])
-        return result['response']
-
+        result = self.submit(
+            "agent:portal:transmitter",
+            {
+                "portal": portal_name,
+                "data_type": data_type,
+                "operation": operation,
+                "params": params,
+            },
+        )
+        if result["status"] == "error":
+            raise PortalConnectionError(result["error"])
+        return result["response"]
 
     def parse_file_text(self, portal_name, data_type, file_id):
         printable = set(string.printable)
-        text = ''
+        text = ""
 
         with temp_dir() as temp:
             file = self.portal_retrieve(portal_name, data_type, file_id)
-            file_type = file['file'].split('.')[-1].lower()
-            file_path = temp.save(base64.b64decode(file['content']), binary = True)
+            file_type = file["file"].split(".")[-1].lower()
+            file_path = temp.save(base64.b64decode(file["content"]), binary=True)
             try:
-                parser = self.manager.get_provider('file_parser', file_type)
+                parser = self.manager.get_provider("file_parser", file_type)
                 file_text = parser.parse(file_path)
                 if file_text:
-                    text = ''.join(filter(lambda x: x in printable, file_text))
+                    text = "".join(filter(lambda x: x in printable, file_text))
 
             except ProviderError as e:
                 pass
         return text
+
+    def parse_file_content(self, portal_name, data_type, file_id):
+        file = self.portal_retrieve(portal_name, data_type, file_id)
+        return base64.b64decode(file["content"])
